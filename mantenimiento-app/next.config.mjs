bef974ddb3372482e7566845ce4fbd4bb570a3a1
edd000 <63064292+edd000@users.noreply.github.com>
2024-06-06 10:51:44 -0500

    ultima Revision

--- mantenimiento-app/next.config.mjs
+++ mantenimiento-app/next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import dotenv from 'dotenv';
+
+// Cargar las variables de entorno
+dotenv.config();
+
+const nextConfig = {
+  // Puedes añadir cualquier configuración de Next.js aquí
+};
 
 export default nextConfig;
+
