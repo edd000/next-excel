9b2b156959008807200f997d713a6d85d5e1e512
edd000 <63064292+edd000@users.noreply.github.com>
2024-06-06 10:48:51 -0500

    Add files via upload

---
+++ mantenimiento-app/index.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image'; // Importa el componente Image de Next.js
+
+export default function Home() {
+  const router = useRouter();
+
+  const handleGoToForm = () => {
+    router.push('/register'); // Redirige a la página del formulario
+  };
+
+  return (
+    <div className="home-container">
+      <Image src="/logo.jpg" alt="Logo" width={400} height={200} />
+      <h1>Bienvenido al Sistema de Solicitudes de Mantenimiento</h1>
+      <button onClick={handleGoToForm} className="go-to-form-button">Ir al Formulario de Registro de Solicitudes</button>
+    </div>
+  );
+}
